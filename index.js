0db2e739856932ea2908e6afdf5eba050891c259
Rishabh Jangid <139529750+Rishabhangid@users.noreply.github.com>
2024-11-04 13:01:43 +0530

    cc

---
+++ index.js
@@ -0,0 +1,25 @@
+// IMOORTING PACKAGES 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// DOTENV FILE IMPORTED
+const dotenv = require("dotenv");
+dotenv.config({path:"./config.env"})
+const PORT = process.env.PORT || 6036;
+
+const CLIENT_URL = process.env.CLIENT_URL;
+// CONNECTING DATABASE & MIDDLEWARES
+const corsOptions = {
+    origin: CLIENT_URL,  // Set this to the URL of your front-end
+    // optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions)); // for hosting porpuse
+// app.use(cors());    
+require("./db/connection");
+app.use(express.json());                // USED TO CONVERT DATA IN JSON
+
+// SERVER TESTING AND INITIALIZING
+app.use(require("./routes/auth"));
+app.get("/",(req,res)=>{ res.send("Hy")})
+app.listen(PORT,()=>{console.log(`Server Started at Port No. ${PORT}`)})
